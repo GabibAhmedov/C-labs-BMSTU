6e81374cf97fd2cae3080736d9a4169514f4c1ea
gabibgit <71562976+gabibgit@users.noreply.github.com>
2020-09-19 20:01:59 +0300

    Add files via upload

---
+++ lab3/lab3.2/lab3.2/lab3.2.cpp
@@ -0,0 +1,50 @@
+﻿
+
+#include <stdio.h>;
+#include <math.h>;
+
+float calculation(float begin, float end, float amount);
+
+int main()
+{
+	float begin, end, accuracy,step,amount,calculationX1,calculationX2 ;
+	scanf_s("%f", &begin);
+	scanf_s("%f", &end);
+	scanf_s("%f", &accuracy);
+	amount = 1;
+	 calculationX2 = calculation(begin, end, amount);
+	do
+	{
+		calculationX1 = calculationX2;
+		amount = amount * 2;
+		calculationX2 = calculation(begin, end, amount);
+
+	} while ((calculationX2 - calculationX1) >= accuracy);
+	printf("%.15f",calculationX2);
+	//printf("%.6f", calculation(begin, end, amount));
+}
+
+float calculation(float begin, float end, float amount)
+{
+	float step, uzel, summchet,summnechet,begincalcu,endcalcu,finalsumm;
+	step = (end - begin) / (2 * amount);
+	begincalcu = log(begin);
+	endcalcu = log(end);
+	summchet = 0;
+	summnechet = 0;
+	for (int i = 1; i < (2 * amount)-1; i = i + 1)
+	{
+		uzel = begin + i * step;
+		if (i % 2 == 0)
+		{
+			summchet = summchet + log(uzel);
+		}
+		else
+		{
+			summnechet = summnechet + log(uzel);
+		}
+	}
+	finalsumm = (step /3) * (begincalcu + (4 * summnechet) + (2 * summchet) + endcalcu);
+	return finalsumm;
+	//printf("%f%f%f%f", begincalcu, summnechet, summchet, endcalcu);
+}
---
+++ lab3/lab3/lab3.cpp
@@ -0,0 +1,39 @@
+﻿
+
+#include <stdio.h>;
+#include <math.h>;
+float factorial(float x);
+int main()
+{
+    float accuracy, n, summIpred,summI,nextposled;
+    scanf_s("%f", &accuracy);
+    n = 1;
+    summI = 1 / factorial(n);
+    do
+    {
+        summIpred = summI;
+        n = n + 1;
+        nextposled = 1 / factorial(n);
+        summI = summI+nextposled;
+        
+    } while (summI - summIpred >= accuracy);
+    printf("%f", summI);
+
+}
+
+
+
+float factorial(float n)
+{
+    float result;
+    if (n == 0)
+    {
+        result = 1;
+    }
+    else
+    {
+        result = n * factorial(n - 1);
+    }
+return result;
+}
+
