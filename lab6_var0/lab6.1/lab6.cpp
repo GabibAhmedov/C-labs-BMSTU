4b618a0c11e84903804d5af40485b251260cb040
gabibgit <71562976+gabibgit@users.noreply.github.com>
2020-10-19 18:15:00 +0300

    Add files via upload

---
+++ lab6_var0/lab6.1/lab6.cpp
@@ -0,0 +1,97 @@
+﻿#include <stdio.h>;
+#include <math.h>;
+#include <malloc.h>
+
+
+int** allocate(int strings, int columns);
+void inputMatrix(int** matrix, int strings, int columns);
+void outputMatrix(int** matrix, int strings, int columns);
+void transport(int** matrix, int strings, int columns);
+
+int main()
+{
+	int strings, number, distance, columns;
+	int** matrix;
+	printf("input strings and colums ");
+	scanf_s("%d", &strings);
+	scanf_s("%d", &columns);
+
+	allocate( strings, columns);
+	matrix= allocate(strings, columns);
+	inputMatrix(matrix, strings, columns);
+	transport( matrix,strings,columns);
+	outputMatrix(matrix, strings, columns);	
+}
+
+int** allocate(int strings, int columns)
+{
+	int* array = (int*) malloc(strings * columns * sizeof(int));
+	int** matrix = (int**)malloc(strings * sizeof(int*));
+	for (int i = 0; i < strings; i++)
+	{
+		matrix[i] = array + i * columns;
+	}
+	return matrix;
+}
+
+void transport(int** matrix, int strings, int columns)
+{
+	int square = abs(strings - columns);
+	if (strings < columns)
+	{
+		for (int i = 0; i < strings; ++i)
+			for (int j = 0; j < columns - i - square; ++j)
+			{
+				int tmp = matrix[i][j];
+				matrix[i][j] = matrix[strings - j - 1][columns - i - 1 - square];
+				matrix[strings - j - 1][columns - i - 1 - square] = tmp;
+			}
+	}
+
+	else if (strings > columns)
+	{
+		for (int i = square; i < strings; ++i)
+			for (int j = 0; j < columns - i + square; ++j)
+			{
+				int tmp = matrix[i][j];
+				matrix[i][j] = matrix[strings - j - 1][columns - i - 1 + square];
+				matrix[strings - j - 1][columns - i - 1 + square] = tmp;
+			}
+	}
+	else
+	{
+
+
+		for (int i = 0; i < strings; ++i)
+			for (int j = 0; j < columns - i; ++j)
+			{
+				int tmp = matrix[i][j];
+				matrix[i][j] = matrix[strings - j - 1][columns - i - 1];
+				matrix[strings - j - 1][columns - i - 1] = tmp;
+			}
+	}
+}
+
+void outputMatrix(int** matrix, int strings, int columns)
+{
+	for (int i = 0; i < strings; i++) 
+	{
+		for (int j = 0; j < columns; j++) 
+		{
+			printf("%d", matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void inputMatrix(int** matrix, int strings, int columns)
+{
+	for (int i = 0; i < strings; i++) 
+	{
+		for (int j = 0; j < columns; j++) 
+		{
+			scanf_s("%d", &matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
---
+++ lab6_var0/lab6.2/lab6.2.cpp
@@ -0,0 +1,111 @@
+﻿#include <stdio.h>;
+#include <math.h>;
+#include <malloc.h>
+
+
+int** allocate(int strings, int columns);
+void inputMatrix(int** matrix, int strings, int columns);
+void outputMatrix(int** matrix, int strings, int columns);
+void swapMinMax(int** matrix, int strings, int columns);
+
+int main()
+{
+	int strings, number, distance, columns;
+	int** matrix;
+	printf("input strings and colums ");
+	scanf_s("%d", &strings);
+	scanf_s("%d", &columns);
+	allocate(strings, columns);
+	matrix = allocate(strings, columns);
+	inputMatrix(matrix, strings, columns);
+	swapMinMax( matrix, strings, columns);
+	outputMatrix(matrix, strings, columns);
+}
+
+int** allocate(int strings, int columns)
+{
+	int* array = (int*)malloc(strings * columns * sizeof(int));
+	int** matrix = (int**)malloc(strings * sizeof(int*));
+	for (int i = 0; i < strings; i++)
+	{
+		matrix[i] = array + i * columns;
+	}
+	return matrix;
+}
+
+void swapMinMax(int** matrix, int strings, int columns)
+{
+	int square = abs(strings - columns);
+	int min = matrix[0][0];
+	int max = matrix[0][0];
+	int maxi = 0; int maxj = 0;
+	int mini = 0; int minj = 0;
+	for (int i = 0; i < strings; ++i)
+	{
+		for (int j = 0; j < columns; ++j)
+		{
+
+			if (matrix[i][j] > max)
+			{
+				max = matrix[i][j]; maxi = i; maxj = j;
+			}
+			if (matrix[i][j] < min)
+			{
+				min = matrix[i][j]; mini = i; minj = j;
+			}
+		}
+	}
+	if (strings < columns) {
+		int tmpMax = max;
+		matrix[maxi][maxj] = matrix[strings - 1 - maxj][columns - 1 - maxi - square];
+		matrix[strings - 1 - maxj][columns - 1 - maxi - square] = tmpMax;
+
+		int tmpMin = min;
+		matrix[mini][minj] = matrix[strings - 1 - minj][columns - 1 - mini - square];
+		matrix[strings - 1 - minj][columns - 1 - mini - square] = tmpMin;
+	}
+	else if (strings > columns)
+	{
+		int tmpMax = max;
+		matrix[maxi][maxj] = matrix[strings - 1 - maxj][columns - 1 - maxi + square];
+		matrix[strings - 1 - maxj][columns - 1 - maxi + square] = tmpMax;
+
+		int tmpMin = min;
+		matrix[mini][minj] = matrix[strings - 1 - minj][columns - 1 - mini + square];
+		matrix[strings - 1 - minj][columns - 1 - mini + square] = tmpMin;
+	}
+	else
+	{
+		int tmpMax = max;
+		matrix[maxi][maxj] = matrix[strings - 1 - maxj][strings - 1 - maxi];
+		matrix[strings - 1 - maxj][strings - 1 - maxi] = tmpMax;
+
+		int tmpMin = min;
+		matrix[mini][minj] = matrix[strings - 1 - minj][strings - 1 - mini];
+		matrix[strings - 1 - minj][strings - 1 - mini] = tmpMin;
+	}
+}
+
+void outputMatrix(int** matrix, int strings, int columns)
+{
+	for (int i = 0; i < strings; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			printf("%d", matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void inputMatrix(int** matrix, int strings, int columns)
+{
+	for (int i = 0; i < strings; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			scanf_s("%d", &matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
